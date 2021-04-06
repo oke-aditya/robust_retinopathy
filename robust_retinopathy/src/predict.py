3e60e46f20ee04b38fef2ed8bfaf1cb71ab6ff30
Aditya Oke <okeaditya315@gmail.com>
2021-04-06 18:46:16 +0530

    add more code

---
+++ robust_retinopathy/src/predict.py
@@ -0,0 +1,20 @@
+import torch
+import config
+import timm
+from PIL import Image
+import torchvision.transforms as T
+
+
+if __name__ == "__main__":
+    model = timm.create_model(config.MODEL_NAME, pretrained=False, num_classes=5)
+    model.load_state_dict(torch.load((config.MODEL_SAVE + "_{epoch}" + ".pt"), map_location="CPU"))
+
+    img = Image.open("file_name")
+
+    img = T.Compose([
+        T.ToTensor(),
+        T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
+
+    out = model(img)
+    print(out)
