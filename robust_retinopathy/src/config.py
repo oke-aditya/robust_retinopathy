a07ef885920d0e5fe3289c09f0319c5c7f9273b7
Aditya Oke <okeaditya315@gmail.com>
2021-05-11 23:50:54 +0530

    split into for validation

--- robust_retinopathy/src/config.py
+++ robust_retinopathy/src/config.py
@@ -3,12 +3,16 @@ TRAIN_DIR = "../../data/train_images/"
 CSV_PATH = "../../data/train.csv"
 MODEL_PATH = "../../data/models/"
 
+TRAIN_SPLIT = 0.8
+VAL_SPLIT = 0.2
 
 LEARNING_RATE = 1e-3
 TRAIN_BATCH_SIZE = 4
 VALID_BATCH_SIZE = 4
+
 NUM_WORKERS = 4
 EPOCHS = 2
+
 IMG_WIDTH = 768
 IMG_HEIGHT = 768
 
--- robust_retinopathy/src/train.py
+++ robust_retinopathy/src/train.py
@@ -21,20 +21,28 @@ if __name__ == "__main__":
         T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ])
 
-    train_dataset = dataset.RetinopathyDataset(config.TRAIN_DIR, config.CSV_PATH, transform=train_trasforms)
-    # model = model.create_model("")
-    train_loader = DataLoader(train_dataset, batch_size=config.TRAIN_BATCH_SIZE,
-                              shuffle=False, num_workers=config.NUM_WORKERS)
+    full_dataset = dataset.RetinopathyDataset(config.TRAIN_DIR, config.CSV_PATH, transform=train_trasforms)
 
-    for batch_idx, (inputs, target) in enumerate(train_loader):
-        print(batch_idx)
-        # print(inputs)
-        print(target)
-        break
+    train_size = int(config.TRAIN_SPLIT * len(full_dataset))
+    test_size = int(config.VAL_SPLIT * len(full_dataset))
+
+    train_dataset, val_dataset = torch.utils.data.random_split(full_dataset, [train_size, test_size])
+
+    train_loader = DataLoader(full_dataset, batch_size=config.TRAIN_BATCH_SIZE,
+                              shuffle=False, num_workers=config.NUM_WORKERS, drop_last=True, pin_memory=True)
+
+    val_loader = DataLoader(val_dataset, batch_size=config.VALID_BATCH_SIZE, shuffle=False,
+                            num_workers=config.NUM_WORKERS, drop_last=True, pin_memory=True)
+
+    # for batch_idx, (inputs, target) in enumerate(train_loader):
+    #     print(batch_idx)
+    #     # print(inputs)
+    #     print(target)
+    #     break
 
     model = timm.create_model(config.MODEL_NAME, pretrained=True, num_classes=5)
 
-    print(model)
+    # print(model)
 
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.AdamW(model.parameters(), lr=config.LEARNING_RATE)
@@ -50,8 +58,7 @@ if __name__ == "__main__":
 
     for epoch in tqdm(range(config.EPOCHS)):
         train_metrics = engine.train_step(model, train_loader, criterion, device, optimizer, scaler=scaler)
-
         val_metrics = engine.val_step(model, val_loader, criterion, device)
 
-        # Save model every epoch
-        torch.save(model.state_dict(), config.MODEL_SAVE + f"_{epoch}" + ".pt")
+    #     # Save model every epoch
+    #     torch.save(model.state_dict(), config.MODEL_SAVE + f"_{epoch}" + ".pt")
